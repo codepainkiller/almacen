c827da7a9dbeffdcda431a463d621c8373a4bea9
Martin Cruz <i@martincruz.me>
2016-12-26 16:34:54 -0500

    categories order by id

--- app/Http/ViewComposers/CategoryComposer.php
+++ app/Http/ViewComposers/CategoryComposer.php
@@ -13,7 +13,7 @@ class CategoryComposer
 
     public function __construct()
     {
-        $this->categories = Category::all();
+        $this->categories = Category::orderBy('id')->get();
     }
 
     public function compose(View $view)
